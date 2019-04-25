9f39d0fdd04c70d19250d7de8cb88f055ff10c1c
D_Garbar <DimaGarbar49@gmail.com>
2019-04-25 08:53:58 +0300

    Finish Rest-api

--- account-rest-api/src/main/java/ua/procamp/dao/impl/InMemoryAccountDao.java
+++ account-rest-api/src/main/java/ua/procamp/dao/impl/InMemoryAccountDao.java
@@ -1,5 +1,8 @@
 package ua.procamp.dao.impl;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Component;
 import ua.procamp.dao.AccountDao;
 import ua.procamp.exception.EntityNotFountException;
 import ua.procamp.model.Account;
@@ -14,27 +17,33 @@ import java.util.Map;
  * <p>
  * todo: 1. Configure a component with name "accountDao"
  */
+@Component("accountDao")
 public class InMemoryAccountDao implements AccountDao {
     private Map<Long, Account> accountMap = new HashMap<>();
+    private AtomicLong last_id = new AtomicLong(0);
 
     @Override
     public List<Account> findAll() {
-        throw new UnsupportedOperationException("Keep calm and implement the method");
+        return new ArrayList<>(accountMap.values());
     }
 
     @Override
     public Account findById(long id) {
-        throw new UnsupportedOperationException("Keep calm and implement the method");
+        return accountMap.get(id);
     }
 
     @Override
     public Account save(Account account) {
-        throw new UnsupportedOperationException("Keep calm and implement the method");
+	    long id = last_id.incrementAndGet();
+	    account.setId(id);
+        accountMap.put(id,account);
+        return account;
     }
 
     @Override
     public void remove(Account account) {
-        throw new UnsupportedOperationException("Keep calm and implement the method");
+    	accountMap.entrySet()
+	        .removeIf(longAccountEntry -> longAccountEntry.getValue().equals(account));
     }
 
     public void clear() {
--- account-rest-api/src/main/java/ua/procamp/web/controller/AccountRestController.java
+++ account-rest-api/src/main/java/ua/procamp/web/controller/AccountRestController.java
@@ -1,6 +1,21 @@
 package ua.procamp.web.controller;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ua.procamp.dao.AccountDao;
+import ua.procamp.dao.impl.InMemoryAccountDao;
+import ua.procamp.model.Account;
 
 /**
  * <p>
@@ -16,6 +31,47 @@ import ua.procamp.dao.AccountDao;
  * todo: 7. Implement method that handles DELETE request with id as path variable removes an account by id
  * todo:    Configure HTTP response status code 204 - NO CONTENT
  */
+@RestController
+@RequestMapping("/accounts")
 public class AccountRestController {
+
+	private InMemoryAccountDao inMemoryAccountDao;
+
+	public AccountRestController(InMemoryAccountDao inMemoryAccountDao) {
+		this.inMemoryAccountDao = inMemoryAccountDao;
+	}
+
+	@GetMapping
+	public List<Account> getAll(){
+		return inMemoryAccountDao.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Account getById(@PathVariable Long id){
+		return inMemoryAccountDao.findById(id);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Account saveAcc(Account account){
+		return inMemoryAccountDao.save(account);
+	}
+
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Account getByIdPut(@PathVariable Long id,Account account){
+		if(account.getId().equals(id)){
+			return inMemoryAccountDao.save(account);
+		}else {
+			throw new IllegalStateException();
+		}
+	}
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void removeAcc(@PathVariable Long id){
+		Optional.ofNullable(inMemoryAccountDao.findById(id))
+			.ifPresent(account -> inMemoryAccountDao.remove(account));
+	}
 
 }
